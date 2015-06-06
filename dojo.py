8966276bf6c557f8023ea3e85f7f071949fa92e1
Steve Holden <steve@holdenweb.com>
2015-06-06 16:47:17 +0100

    Tidy up comments and layout.

--- dojo.py
+++ dojo.py
@@ -1,6 +1,5 @@
 #
-# TODO: Add some means of ignoring cards already matched
-#       Add sound effects
+# TODO: Add sound effects
 #       Ignore clicks after displaying second card until
 #       either hit or miss is reported.
 #       Consider re-casting the data structure as a dict
@@ -18,41 +17,36 @@ import time
 COLS = 4
 ROWS= 3
 IMSIZE = 200
-STATUS = []    # cells that have been clicked on
-ignore = []   # cells that have been matches and are no longer in play
+STATUS = []		# cells that have been clicked on
+ignore = []		# cells that have been matches and are no longer in play
 
+# Create two of each card image, then randomize before creating the board
 START_IMAGES= [ "im"+str(i+1) for i in range(COLS*ROWS//2)]*2
 random.shuffle(START_IMAGES)
 
-
 STATUS=[]
 
-board = []
+board = []					# initialize the board
 for row in range(ROWS):
-	new_entry=[]
+	new_row=[]
 	for col in range(COLS):
 		image_name = START_IMAGES.pop()
 		temp=Actor(image_name, (col*IMSIZE, row*IMSIZE))
-		temp.image_name = image_name
+		temp.image_name = image_name # used to verify matches
 		temp.topleft=(col*IMSIZE, row*IMSIZE)
-		new_entry.append(temp)
+		new_row.append(temp)
+	board.append(new_row)
 
-		#STATUS.append([tileId,0])
-
-		# print (col*IMSIZE, row*IMSIZE)
-		# board[row][col].topleft = (col*IMSIZE, row*IMSIZE)
-	board.append(new_entry)
-
-def draw():
+def draw():					# draw the board when pygame-zero says to
 	screen.clear()
 	for row in range(ROWS):
 		for col in range(COLS):
-			if (row, col) in ignore:
+			if (row, col) in ignore:	# already matched
 				checkmark.topleft = IMSIZE*col, IMSIZE*row
 				checkmark.draw()
-			elif (row, col) in STATUS:
+			elif (row, col) in STATUS:	# clicked this move: show face
 				board[row][col].draw()
-			else:
+			else:						# regular clickable card
 				steve.topleft = IMSIZE*col, IMSIZE*row
 				steve.draw()
 
@@ -75,13 +69,13 @@ def on_mouse_down(pos, button):
 		coords = findTile(pos)
 		if coords not in STATUS:
 			STATUS.append(coords) # now they are
-			if len(STATUS) == 1:  # ignore first click
+			if len(STATUS) == 1:  # 1st click - turn not yet over
 				pass
-			elif len(STATUS) == 2: # second click - check for match
+			elif len(STATUS) == 2: # 2nd click - check for match
 				(x1, y1), (x2, y2) = STATUS # an "unpacking assignment"
 				if board[x1][y1].image_name == board[x2][y2].image_name:
 					print("Success sound")
-					# add coords to "ignore" list
+					# add cards to list of non-clickable positions
 					for pos in STATUS:
 						ignore.append(pos)
 				else:
